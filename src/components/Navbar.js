edcb69494181bd14e09e9fa15c6a5c9bceb68f12
Yan <yan@codevelop.ch>
2019-02-13 10:26:01 +0100

    quickfixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,19 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+// import logo from '../img/logo.svg'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+          <h1 className="has-text-weight-bold is-size-2">Christine Abbt</h1>
         </Link>
       </div>
       <div className="navbar-start">
         <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
+          <h1 className="has-text-weight-bold is-size-4">About</h1>
         </Link>
       </div>
       <div className="navbar-end">
@@ -29,9 +24,7 @@ const Navbar = () => (
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
+
         </a>
       </div>
     </div>
